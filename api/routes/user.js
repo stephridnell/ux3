340048d10e948d906139b399c253a91f39f0642b
Steph Ridnell <steph.ridnell@gmail.com>
2021-09-13 09:49:05 +1000

    Updated error status

--- api/routes/user.js
+++ api/routes/user.js
@@ -14,17 +14,16 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const id = req.params.id
-  const notFoundMsg = `User does not exist`
 
   try {
     const user = await User.findById(id)
     if (!user) {
-      res.status(404).send(notFoundMsg)
+      res.status(404).send(`User does not exist`)
       return
     }
     res.json(user)
   } catch (err) {
-    res.status(404).send(notFoundMsg)
+    res.status(500).send(err.message)
   }
 })
 
