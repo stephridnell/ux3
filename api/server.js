f1857bfe53bfa8a2d4da4b2699bc7b0178ce27d8
Steph Ridnell <steph.ridnell@gmail.com>
2021-09-13 14:08:19 +1000

    Removed deprecated package

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
 // dependencies
 require('dotenv').config()
-const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -11,8 +10,8 @@ connectDatabase()
 
 // express app setup
 const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('*', cors())
 
 // routes
